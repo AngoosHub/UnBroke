d7852929f34bc5a14742be7c081a575892ab83b2
Angus Lin <anguslininfo@gmail.com>
2018-12-03 13:03:48 -0800

    login update

--- student/Launch_Login_Signup/new-login/app.js
+++ student/Launch_Login_Signup/new-login/app.js
@@ -50,6 +50,10 @@ function setup() {
         //Sign in
         const promise = auth.signInWithEmailAndPassword(email, pass);
         promise.catch(e => console.log(e.message));
+        promise.then(function () {
+            window.location.href = "../../Student Home/studenthome.html";
+        });
+
     });
 
     txtLogin[1].addEventListener('click', e => {
@@ -59,6 +63,9 @@ function setup() {
         const auth = firebase.auth();
         // Sign in
         const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.then(function () {
+            window.location.href = "../../../business/Business Home/index.html";
+        });
         promise.catch(e => console.log(e.message));
     });
 
@@ -68,8 +75,17 @@ function setup() {
         const pass = txtPassword[0].value;
         const auth = firebase.auth();
         // Sign Up
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
+        auth.createUserWithEmailAndPassword(email, pass)
+
+            .then(function () {
+                auth.signInWithEmailAndPassword(email, pass)
+                    .then(function () {
+                        window.location.href = "../../Student Home/studenthome.html";
+                    })
+                    .catch(e => console.log(e.message));
+            })
+            .catch(e => console.log(e.message));
+
     });
 
     txtSignUp[1].addEventListener('click', e => {
@@ -77,26 +93,49 @@ function setup() {
         const email = txtEmail[1].value;
         const pass = txtPassword[1].value;
         const auth = firebase.auth();
-        // Sign Up
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
 
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode);
-            console.log(errorMessage);
-        });
+        // Sign Up
+        auth.createUserWithEmailAndPassword(email, pass)
+
+            .then(function () {
+                auth.signInWithEmailAndPassword(email, pass)
+                    .then(function () {
+                        window.location.href = "../../../business/Business Home/index.html";
+                    })
+                    .catch(e => console.log(e.message));
+            })
+            .catch(e => console.log(e.message));
+
+
+        // Old Sign Up Code
+        // const promise = auth.createUserWithEmailAndPassword(email, pass);
+        // promise.catch(e => console.log(e.message));
+
+        // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        //     // Handle Errors here.
+        //     var errorCode = error.code;
+        //     var errorMessage = error.message;
+        //     console.log(errorCode);
+        //     console.log(errorMessage);
+        // });
+
+        // promise.then(function () {
+        //     const promise1 = auth.signInWithEmailAndPassword(email, pass);
+        //     promise1.catch(e => console.log(e.message));
+        //     promise1.then(function () {
+        //         window.location.href = "../../../business/Business Home/index.html";
+        //     });
+        // });
 
     });
 
-    
+
 
     // Add a realtime listener
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
             console.log(firebaseUser);
+            alert("Login Success!");
         } else {
             console.log('not logged in');
         }
@@ -139,14 +178,47 @@ var uiConfig = {
 
 ui.start('#firebaseui-auth-container', uiConfig);
 
+// Initialize the FirebaseUI Widget using Firebase.
+var ui1 = new firebaseui.auth.AuthUI(firebase.auth());
 
-const txtEmail = document.getElementsByClassName('txtEmail');
-const txtPassword = document.getElementsByClassName('txtPassword');
-const txtLogin = document.getElementsByClassName('txtLogin');
-// const txtSignUp = document.getElementsByClassName('txtSignUp');
+var uiConfig1 = {
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader1').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '../../Student Home/studenthome.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID //,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+};
 
-var email = txtEmail[0].value;
-var password = txtPassword[0].value;
+ui1.start('#firebaseui-auth-container1', uiConfig1);
+
+
+// const txtEmail = document.getElementsByClassName('txtEmail');
+// const txtPassword = document.getElementsByClassName('txtPassword');
+// const txtLogin = document.getElementsByClassName('txtLogin');
+// // const txtSignUp = document.getElementsByClassName('txtSignUp');
+
+// var email = txtEmail[0].value;
+// var password = txtPassword[0].value;
 
 
 
