e975a1e58eb13e3d15c24810d0f8dbc69382381e
Eugene Lee <leeeugene2000@gmail.com>
2018-12-03 20:19:51 -0800

    couponlist changes

--- student/Student Home/main1.js
+++ student/Student Home/main1.js
@@ -11,40 +11,78 @@ function refData(data) {
   $("body").append(newBodyEl);
 
   var businesses = data.val();
-  var dosTacos = businesses["Dos Tacos"]["Coupons"];
+  var dosTacos = businesses["Dos Tacos"];
+  var coupons = businesses["Dos Tacos"]["Coupons"];
 
-  var keys = Object.keys(dosTacos);
+  var keys = Object.keys(coupons);
+
+  var current = $('<div/>').addClass('current').html('<p>Restaurants</p>');
+  $(newBodyEl).append(current);
 
   for (var i = 0; i < keys.length; i++) {
     var k = keys[i];
 
     var outer = $("<div/>").addClass("ad").attr('id', k);
 
-    var title = $("<div/>").addClass("title").html("<p>" + dosTacos[k]["Name"] + "</p>");
+    var title = $("<div/>").addClass("title").html("<p>" + dosTacos["Restaurant"] + "</p>");
     outer.append(title);
 
-    var dis = $("<div/>").addClass("discription").html("<p>" + dosTacos[k]["Deal"] + "</p>");
+    var dis = $("<div/>").addClass("discription").html("<p>" + dosTacos["Location"] + "</p>");
     outer.append(dis);
 
     var dates = $("<div/>").addClass("dates");
 
-    var start = $("<div/>").addClass("startDate");
-    var p1 = $("<p/>").addClass("dateTitle").html("Start Date:");
-    var p2 = $("<p/>").addClass("contentDate").html(dosTacos[k]["Start_Date"]);
-    start.append(p1).append(p2);
-    dates.append(start);
+    var image = $("<div/>").addClass("imageDiv").html('<img src="' + coupons[k]["url"] + '">');
+    dis.append(image);
 
-    var start = $("<div/>").addClass("endDate");
-    var p3 = $("<p/>").addClass("dateTitle").html("End Date:");
-    var p4 = $("<p/>").addClass("contentDate").html(dosTacos[k]["End_Date"]);
-    start.append(p3).append(p4);
-    dates.append(start);
-    outer.append(dates);
+    var cuisine = $("<div/>").addClass("cuisine").html("<p>" + dosTacos["Cuisine"] + "</p>");
+    dis.append(cuisine);
 
-    var print = $('<button/>').addClass('print').html('<p>Print</p>')
-    window.print();
-    outer.append(dlt);
+    var price = $("<div/>").addClass("price").html("<p>" + dosTacos["Price"] + "</p>");
+    dis.append(price);
+
+    var reviews = $("<div/>").addClass("reviews").html("<p>" + dosTacos["Reviews"] + "</p>");
+    dis.append(reviews);
+
+    var dlt = $('<button/>').addClass('delete').html('<p>View</p>');
+    dis.append(dlt);
+
+    var edit = $('<button/>').addClass('edit').html('<p>Like</p>');
+    dis.append(edit); 
 
     $(".content").append(outer);
   }
+
+  var deleteBtn = $("button.delete");
+  for (var i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener('click', function() {
+      window.location.href = "couponlist.html";
+    }) 
+    }
+  }
+
+  var editBtn = $("button.edit");
+  for (var i = 0; i < editBtn.length; i++) {
+    editBtn[i].addEventListener('click', pressEdit)
+  }
+
+var goHome = document.getElementById("menuImage");
+goHome.addEventListener("click", function () {
+  window.location.href = "../Create Post/index.html";
+})
+
+function pressDelete(x) {
+  var coupons = database.ref("businesses/Dos Tacos/Coupons");
+  var id = x.target.parentElement.parentElement.getAttribute('id');
+  coupons.child(id).remove();
+}
+
+function pressEdit(x) {
+  var coupons = database.ref("businesses/Dos Tacos/Coupons");
+  var id = x.target.parentElement.parentElement.getAttribute('id');
+  var current = {
+    id: id
+  };
+  database.ref('businesses/current').update(current);
+  window.location.href = "../Edit Post/index.html";
 }
---
+++ student/Student Home/main2.js
@@ -0,0 +1,91 @@
+var ref = database.ref("businesses");
+
+ref.on("value", refData);
+
+
+
+function refData(data) {
+
+  $(".content").remove();
+  var newBodyEl = $("<div/>").addClass("content");
+  $("body").append(newBodyEl);
+
+  var businesses = data.val();
+  var dosTacos = businesses["Dos Tacos"]["Coupons"];
+
+  var keys = Object.keys(dosTacos);
+
+  var current = $('<div/>').addClass('current').html('<p>Current Advertisements</p>');
+  $(newBodyEl).append(current);
+
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+
+    var outer = $("<div/>").addClass("ad").attr('id', k);
+
+    var title = $("<div/>").addClass("title").html("<p>" + dosTacos[k]["Name"] + "</p>");
+    outer.append(title);
+
+    var dis = $("<div/>").addClass("discription").html("<p>" + dosTacos[k]["Deal"] + "</p>");
+    outer.append(dis);
+
+    var dates = $("<div/>").addClass("dates");
+
+    var start = $("<div/>").addClass("startDate");
+    var p1 = $("<p/>").addClass("dateTitle").html("Start Date:");
+    var p2 = $("<p/>").addClass("contentDate").html(dosTacos[k]["Start_Date"]);
+    start.append(p1).append(p2);
+    dates.append(start);
+
+    var start = $("<div/>").addClass("endDate");
+    var p3 = $("<p/>").addClass("dateTitle").html("End Date:");
+    var p4 = $("<p/>").addClass("contentDate").html(dosTacos[k]["End_Date"]);
+    start.append(p3).append(p4);
+    dates.append(start);
+    outer.append(dates);
+
+    var image = $("<div/>").addClass("imageDiv").html('<img src="' + dosTacos[k]["url"] + '">');
+    dis.append(image);
+
+    var dlt = $('<button/>').addClass('delete').html('<p>Add to Coupon</p>');
+    dis.append(dlt);
+
+    var edit = $('<button/>').addClass('edit').html('<p>Edit</p>');
+    dis.append(edit);
+
+    $(".content").append(outer);
+  }
+
+  var deleteBtn = $("button.delete");
+  for (var i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener('click', function() {
+        window.location.href = "couponbook.html";
+    })
+  }
+
+  var editBtn = $("button.edit");
+  for (var i = 0; i < editBtn.length; i++) {
+    editBtn[i].addEventListener('click', pressEdit)
+  }
+}
+
+var goHome = document.getElementById("menuImage");
+goHome.addEventListener("click", function () {
+  window.location.href = "../Create Post/index.html";
+})
+
+function pressDelete(x) {
+  var dosTacos = database.ref("businesses/Dos Tacos/Coupons");
+  var id = x.target.parentElement.parentElement.getAttribute('id');
+  dosTacos.child(id).remove();
+}
+
+function pressEdit(x) {
+  var dosTacos = database.ref("businesses/Dos Tacos/Coupons");
+  var id = x.target.parentElement.parentElement.getAttribute('id');
+  var current = {
+    id: id
+  };
+  database.ref('businesses/current').update(current);
+  window.location.href = "../Edit Post/index.html";
+}
